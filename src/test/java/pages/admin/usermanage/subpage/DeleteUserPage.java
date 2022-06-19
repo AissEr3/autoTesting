173f5bfea9ed5010d37ed77769087c62ff4a8bc0
AissEr3 <66399342+AissEr3@users.noreply.github.com>
2022-06-19 21:34:49 +0800

    Update DeleteUserPage.java

--- src/test/java/pages/admin/usermanage/subpage/DeleteUserPage.java
+++ src/test/java/pages/admin/usermanage/subpage/DeleteUserPage.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebDriver;
 import pages.admin.usermanage.base.MessagePage;
 
 public class DeleteUserPage extends MessagePage {
+    
     public DeleteUserPage(WebDriver fatherPageDriver){
         super(fatherPageDriver);
     }
+    
 }
